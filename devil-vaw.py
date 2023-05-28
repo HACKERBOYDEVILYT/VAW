3419557e88975d0417fdccb0e0f9e8be15499c30
DEVIL BOY <94786786+HACKERBOYDEVILYT@users.noreply.github.com>
2023-05-28 12:59:24 +0600

    Update devil-vaw.py

--- devil-vaw.py
+++ devil-vaw.py
@@ -1317,7 +1317,7 @@ def Subscraption():
 	key1=open('/data/data/com.termux/files/usr/bin/.mrBALOCH -cov', 'r').read()
 	clear()
 	print(logo)
-	r1=requests.get("https://github.com/HACKERBOYDEVILYT/Test/tree/main").text
+	r1=requests.get("https://github.com/HACKERBOYDEVILYT/Test/tree/main/TEST.txt").text
 	if key1 in r1:
 		os.system('clear')
 		print(logo)
